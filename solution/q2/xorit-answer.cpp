8b40afc4b27d760e8334302faf8798820bb4a9bf
Kunal Tawatia <39331243+kunaltawatia@users.noreply.github.com>
2018-11-30 19:55:17 +0530

    Update xorit-answer.cpp

--- solution/q2/xorit-answer.cpp
+++ solution/q2/xorit-answer.cpp
@@ -1,9 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
+ll popcount(ll n)
+{
+	ll ans=0;
+	while(n)
+	{
+		ans+=n%2;
+		n /=2;
+	}
+	return ans;
+}
 int main()
 {
 	ll n;
 	cin>>n;
-	cout<<pow(2, __builtin_popcount(n));
+	cout<<pow(2,popcount(n));
 }
